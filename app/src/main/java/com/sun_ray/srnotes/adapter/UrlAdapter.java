58a54459bbe3a2364ae1758e240013ea3542d92a
surya <suryakashakti@gmail.com>
2024-08-02 11:34:18 +0530

    Done

--- app/src/main/java/com/sun_ray/srnotes/adapter/UrlAdapter.java
+++ app/src/main/java/com/sun_ray/srnotes/adapter/UrlAdapter.java
@@ -7,7 +7,6 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,19 +46,13 @@ public class UrlAdapter extends RecyclerView.Adapter<UrlAdapter.ViewHolder> {
         holder.title.setText(list.get(position).getTitle());
         holder.url.setText(list.get(position).getLink());
         holder.date.setText(list.get(position).getDate());
-        try {
-            holder.url.setSelected(true);
-            holder.title.setSelected(true);
-        } catch (Exception e){
-            Toast.makeText(context, "Error: "+e, Toast.LENGTH_LONG).show();
-        }
 
         holder.cardView.setOnLongClickListener(v -> {
             DeleteItem(position);
             return true;
         });
 
-        holder.Open.setOnClickListener(v -> {
+        holder.cardView.setOnClickListener(v -> {
             try {
                 String Url = list.get(position).getLink();
                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(Url));
@@ -73,9 +66,9 @@ public class UrlAdapter extends RecyclerView.Adapter<UrlAdapter.ViewHolder> {
     private void DeleteItem(int position) {
         boolean a = true;
         AlertDialog dialog = new AlertDialog.Builder(context)
-                .setTitle("Delete")
+                .setTitle("Delete : "+list.get(position).getTitle())
                 .setIcon(R.drawable.ic_delete)
-                .setMessage("Are You Sure Want To Delete ?")
+                .setMessage("Are You Sure Want To Delete ?\n\n"+list.get(position).getLink())
                 .setPositiveButton("Yes", (dialog1, which) -> {
                     database.urlDao().deleteUrl(new Url(
                             list.get(position).getId(),
@@ -98,13 +91,11 @@ public class UrlAdapter extends RecyclerView.Adapter<UrlAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         LinearLayout cardView;
-        Button Open;
         TextView title, url, date;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             cardView = itemView.findViewById(R.id.Url_item);
-            Open = itemView.findViewById(R.id.open);
             title = itemView.findViewById(R.id.title);
             url = itemView.findViewById(R.id.url);
             date = itemView.findViewById(R.id.date);
